e2d515887c086448620c7c896ae10b79a69b079b
waziri123 <waziriallyamiri123@gmail.com>
2023-10-29 06:55:15 -0700

    step4 modification

--- resources/views/components/accordion-wrapper.blade.php
+++ resources/views/components/accordion-wrapper.blade.php
@@ -1,10 +1,12 @@
-@props(['tab' => '', 'ref' => ''])
+@props(['tab' => '', 'ref' => '','selected'=>''])
 
-<div {{$attributes->merge(['class' => 'relative overflow-hidden transition-all max-h-0 duration-700', 'style' => '']) }} 
+<div  {{$attributes->merge(['class' => 'relative overflow-scroll transition-all max-h-0 duration-700', 'style' => '']) }} 
     x-ref="{{$ref}}" 
-    x-bind:style="selected == {{$tab}} ? 'max-height: ' + $refs.{{$ref}}.scrollHeight + 'px' : ''"
+   
+    x-bind:style="selected == {{$tab}} ? 'max-height: ' + 100 + '%' : ''"
+    
 >
     <div class="p-4 border-2 border-gray-300">
-        {{$slot}}
+        {{$slot}} 
     </div>
 </div>
